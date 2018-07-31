cebdb42ec80a26ba83d148fe04c29a5d3a5e3a04
mudit jain <mudit.jain@lmsin.com>
2018-07-31 18:18:25 +0530

    added

---
+++ TestaPOPProject/TestaPOPProject/Service.swift
@@ -0,0 +1,17 @@
+//
+//  Service.swift
+//  TestaPOPProject
+//
+//  Created by LMS on 31/07/18.
+//  Copyright © 2018 LMS. All rights reserved.
+//
+
+import Foundation
+class Service{
+  
+    public func displayName()->String
+    {
+     return "Hi this is my pop"
+    }
+    
+}
---
+++ TestaPOPProject/TestaPOPProject/TestaPOPProject.h
@@ -0,0 +1,19 @@
+//
+//  TestaPOPProject.h
+//  TestaPOPProject
+//
+//  Created by LMS on 31/07/18.
+//  Copyright © 2018 LMS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestaPOPProject.
+FOUNDATION_EXPORT double TestaPOPProjectVersionNumber;
+
+//! Project version string for TestaPOPProject.
+FOUNDATION_EXPORT const unsigned char TestaPOPProjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestaPOPProject/PublicHeader.h>
+
+
